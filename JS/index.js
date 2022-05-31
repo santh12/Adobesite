3699b95a5e0468a731ace9e52cdd96a185e989c6
santh12 <59067404+santh12@users.noreply.github.com>
2022-05-31 18:55:35 +0530

    Update index.js
    Product Teaser Js

--- JS/index.js
+++ JS/index.js
@@ -29,3 +29,23 @@ function closeNav() {
     document.getElementById("logo").style.visibility="visible";
     document.getElementById("custom--desktop--hide").style.marginLeft="222px";
 }
+
+//Start for Product teaser slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("cust-mobile-scroll");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+//End for Product teaser slider
