e17a941570ca04ea9d42d938d474b38ab4002c39
santh12 <59067404+santh12@users.noreply.github.com>
2022-06-03 23:19:29 +0530

    Update index.js
    changes done

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,3 @@
-
 document.getElementById("closebtn").style.visibility="hidden";
 document.body.style.overflowX = "hidden";
 function openNav() {
@@ -11,7 +10,15 @@ function openNav() {
     document.getElementById("heroBannerDiv").style.display="none";
     document.getElementById("secBannerborder").style.display="none";
     document.getElementById("newslettercontainer").style.display="none";
-    document.getElementById("featurecontainer").style.display="none"; 
+    document.getElementById("featurecontainer").style.display="none";
+    document.getElementById("product-teaser-small-bg").style.display="none"; 
+    document.getElementById("teaser").style.display="none"; 
+    document.getElementById("product-teaser-small-big").style.display="none"; 
+    document.getElementById("teaser-two").style.display="none"; 
+    document.getElementById("product-teaser-small-two").style.display="none";
+    document.getElementById("image-tiles").style.display="none";
+    document.getElementById("newslettercontainer").style.display="none";
+    document.getElementById("footer").style.display="none";
     document.getElementById("myTopnav").style.backgroundColor="#F7F7F7"
     document.getElementById("custom--desktop--hide").style.marginLeft="0px";
     document.getElementById("headerfont").style.visibility="visible";
@@ -23,11 +30,17 @@ function closeNav() {
     document.getElementById("heroBannerDiv").style.display="";
     document.getElementById("secBannerborder").style.display="";
     document.getElementById("newslettercontainer").style.display="";
-    document.getElementById("featurecontainer").style.display=""; 
+    document.getElementById("featurecontainer").style.display="";
+    document.getElementById("product-teaser-small-bg").style.display=""; 
+    document.getElementById("teaser").style.display=""; 
+    document.getElementById("product-teaser-small-big").style.display=""; 
+    document.getElementById("teaser-two").style.display=""; 
+    document.getElementById("product-teaser-small-two").style.display="";
+    document.getElementById("image-tiles").style.display="";
+    document.getElementById("newslettercontainer").style.display="";
+    document.getElementById("footer").style.display=""; 
     document.getElementById("main").style.visibility="visible";
     document.getElementById("closebtn").style.visibility="hidden";
     document.getElementById("logo").style.visibility="visible";
     document.getElementById("custom--desktop--hide").style.marginLeft="222px";
 }
-
-
